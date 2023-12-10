c609077bd92942af2545f76f892f2c8f070b7004
GustavoCoelho295 <iago.pinheirodeoliveira@gmail.com>
2023-12-10 17:34:43 -0300

    adicionado titulo e logo na aba

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,8 @@ class NewDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+          <link rel="icon" href="/ifcee.png" />
+          <title>apredizagem de inglês</title>
         </Head>
         <body>
           <Main />
